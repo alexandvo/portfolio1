685dd29bc1c5c15df05882960265dbff108cb51f
Alex Vo <alexanvo@tamu.edu>
2023-10-15 03:42:06 -0500

    all changes

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+function updateHomeButton() {
+    const homeButton = document.getElementById('home-button');
+    const innerCircle = document.getElementById('inner');
+    const yOffset = window.scrollY;
+    if (yOffset == 0) {
+        homeButton.style.pointerEvents = 'none';
+        innerCircle.style.background = '#5f5f5f';
+    } else {
+        homeButton.style.pointerEvents = 'auto';
+        innerCircle.style.background = '#9e0000';
+    }
+}
+
+window.addEventListener('load', updateHomeButton);
+window.addEventListener('scroll', updateHomeButton);
 
