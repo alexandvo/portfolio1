d250e1246e47f5d13ab7e28062d59595a5f03a7a
Alex Vo <alexanvo@tamu.edu>
2023-10-17 03:35:20 -0500

    adding clouds

--- main.js
+++ main.js
@@ -10,7 +10,19 @@ function updateHomeButton() {
         innerCircle.style.background = '#9e0000';
     }
 }
+window.addEventListener('scroll', () => {
+    const cloud1 = document.getElementById('cloud-wrapper1');
+    const cloud2 = document.getElementById('cloud-wrapper2');
+    const cloud3 = document.getElementById('cloud-wrapper3');
+    const cloud4 = document.getElementById('cloud-wrapper4');
+    const cloud5 = document.getElementById('cloud-wrapper5');
+    const cloud6 = document.getElementById('cloud-wrapper6');
+    const yOffset = window.scrollY;
+    cloud1.style.transform = `scale(0.8) translateY(-${yOffset * 0.43}px)`;
+    cloud2.style.transform = `translateY(${yOffset * 0.2}px)`;
+    cloud3.style.transform = `scaleY(0.6) translateY(-${yOffset * 0.6}px)`;
 
+});
 window.addEventListener('load', updateHomeButton);
 window.addEventListener('scroll', updateHomeButton);
 
